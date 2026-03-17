0760b34c89607c115d978bab0288611d0d1e1673
ricothenfx <ricothenfx@gmail.com>
2026-03-17 13:57:20 +0700

    initial commit - MediaHub-FastAPI

---
+++ app/db.py
@@ -0,0 +1,61 @@
+import os
+from collections.abc import AsyncGenerator
+#from sqlalchemy import UUID
+import uuid
+
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime, UTC
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi import Depends
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship(
+        "Post",
+        back_populates="user",
+        cascade="all, delete"
+    )
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(String(36), ForeignKey("user.id"), nullable=False)
+    caption = Column(Text)
+    url = Column(String, nullable=False)
+    file_type = Column(String, nullable=False)
+    file_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(UTC))
+
+    user = relationship("User", back_populates="posts")
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
